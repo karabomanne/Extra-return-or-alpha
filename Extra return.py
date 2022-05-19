fd98c3db58ba1beffd11cf6eda3ac97b2f0e6efd
karabomanne <105419369+karabomanne@users.noreply.github.com>
2022-05-19 13:09:23 +0200

    Update Extra return.py

--- Extra return.py
+++ Extra return.py
@@ -1,8 +1,8 @@
 #Extra return or alpha
-b = 0.8
-i = 0.05
-m = 0.07
-Rp = 0.09
+b = 0.8 #Beta on investment
+i = 0.05 #Risk-free interest 
+m = 0.07 #Return on the market
+Rp = 0.09 #Actual expected return
 
 alpha = Rp - i - b * (m - i)
 print ("The manager's alpha is{:0.2f}%".format (alpha))
